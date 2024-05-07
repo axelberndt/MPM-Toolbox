8fe6052da03d70427eda86fba4d54be82cad60c9
Axel Berndt <axel.berndt@uni-paderborn.de>
2024-05-07 13:55:01 +0200

    v0.1.26 - Dependency updates   - WebLaF Core v1.2.13 to v1.2.14   - WebLaF UI v1.2.13 to v1.2.14   - junit Jupiter API v5.10.2   - xstream v1.4.20

--- src/mpmToolbox/Main.java
+++ src/mpmToolbox/Main.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  * @author Axel Berndt
  */
 public class Main {
-    public static final String version = "0.1.25";
+    public static final String version = "0.1.26";
 
     public static void main(String[] args) {
         // read the application settings from file
